101bd0a73aec94d4140c5948c6c4eda86ddc79ba
Ildar Sakaev <ildar_cb@mail.ru>
2017-10-09 22:56:27 +0500

    Доработано верхнее меню

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
 var toggle_button = document.querySelector(".page-header__toggle");
 var popup = document.querySelector(".main-nav");
-var page_header_wrapper = document.querySelector(".page-header__logo-wrapper");
+var page_header_heading = document.querySelector(".page-header__heading");
 var page_header = document.querySelector(".page-header");
+var page_header_menu = document.querySelector(".page-header__menu");
 
 document.addEventListener("DOMContentLoaded", function (evt) {
   evt.preventDefault();
   popup.classList.remove("main-nav--visible");
-  page_header_wrapper.classList.remove("page-header__logo-wrapper--background");
-  page_header.classList.remove("page-header--position");
-  page_header.classList.remove("page-header--add-picture-position");
+  page_header_heading.classList.remove("page-header__heading--background");
+  page_header_menu.classList.remove("page-header__menu--position");
+  page_header.classList.remove("page-header--margin");
   toggle_button.classList.remove("page-header__toggle--open");
 });
 
 toggle_button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("main-nav--visible");
-  page_header_wrapper.classList.toggle("page-header__logo-wrapper--background");
-  page_header.classList.toggle("page-header--position");
-  page_header.classList.toggle("page-header--add-picture-position");
+  page_header_heading.classList.toggle("page-header__heading--background");
+  page_header_menu.classList.toggle("page-header__menu--position");
+  page_header.classList.toggle("page-header--margin");
 /*  if (toggle_button.classList.contains("page-header__toggle--open")) {
     toggle_button.classList.remove("page-header__toggle--open");
   };*/
